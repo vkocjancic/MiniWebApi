ae8ffac7c03cbf70ee9db1dcdd3125ab1f57ae85
Vladimir Kocjancic <vkocjancic@gmail.com>
2016-08-29 22:57:43 +0200

    Initial commit

---
+++ MiniWebApi/Controllers/PingController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MiniWebApi.Controllers
+{
+    public class PingController : ApiController
+    {
+
+        public HttpResponseMessage Get()
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, (new { active = true }));
+        }
+
+    }
+}
---
+++ MiniWebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiniWebApi.Global" Language="C#" %>
